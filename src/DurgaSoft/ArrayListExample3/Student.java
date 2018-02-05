40765b739245174f703b9aac65224a900cc57160
rahul kumar <user10@sunrichtech.com>
2018-02-05 16:37:11 +0530

    ArrayList alongwith example is commited

---
+++ src/DurgaSoft/ArrayListExample3/Student.java
@@ -0,0 +1,18 @@
+package DurgaSoft.ArrayListExample3;
+
+/**
+ * Created by Rahul Yadav
+ * User defined Class-object in java ArrayList
+ *
+ */
+public class Student {
+    int rollNo;
+    String name;
+    int age;
+
+    public Student(int rollNo, String name, int age) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/DurgaSoft/ArrayListExample4.java
@@ -0,0 +1,46 @@
+package DurgaSoft;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by Rahul Yadav
+ * addAll(Collection c) method
+ * removeAll() method
+ * retainAll() method
+ */
+
+public class ArrayListExample4 {
+    public static void main(String[] args) {
+        System.out.println("-----------------addAll method-------------");
+        ArrayList<String> arrayList=new ArrayList<String>();
+        arrayList.add("rahul");
+        arrayList.add("rajesh");
+        arrayList.add("rohit");
+
+        ArrayList<String> arrayList1=new ArrayList<String>();
+        arrayList1.add("bhavik");
+        arrayList1.add("pushkar");
+        arrayList.addAll(arrayList1);//added second list in first list
+
+        Iterator iterator=arrayList.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+
+        System.out.println("----------------removeAll() method-----------");
+        arrayList.removeAll(arrayList1);//removed arrayList1 form the arrayList
+
+        Iterator iterator1=arrayList.iterator();
+        while (iterator1.hasNext()){
+            System.out.println(iterator1.next());
+        }
+
+
+        System.out.println("--------------------------------------------");
+
+
+
+    }
+}
