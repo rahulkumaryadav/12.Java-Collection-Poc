10eb0c9d23d8c94cfa0dda7f11d5570586e4a782
Rahul kumar <rahulkumar96369@gmail.com>
2018-02-07 20:46:50 +0530

    cursor is commited

---
+++ src/DurgaSoft/Cursors.java
@@ -0,0 +1,17 @@
+package DurgaSoft;
+
+/**
+ * @Author Rahul Yadav
+ * @Dated :208-02-07
+ * If we want to get object one by one from the collection then we should go for cursor
+ * 3 types of cursor in java
+ * Enumeration
+ * Iterator
+ * ListIterator
+ */
+
+public class Cursors {
+    public static void main(String[] args) {
+        //TODO page no 23
+    }
+}
