da9d6344a0fed142bce38006adeaa877059bfb8a
Rahul kumar <rahulkumar96369@gmail.com>
2018-02-17 20:30:53 +0530

    synchronized verson of hash map added in todo list

---
+++ src/DurgaSoft/Map/HashMap/HashMapExample2.java
@@ -0,0 +1,14 @@
+package DurgaSoft.Map.HashMap;
+
+/**
+ * @Author Rahul Yadav
+ * Dated: 2018-02-17
+ * By default HashMap object is not synchronized but we can get synchronized version by using Synchronized version of collection of collection class
+ * Map m = Collections.synchoronizedMap(HashMap m);
+ */
+
+public class HashMapExample2 {
+    public static void main(String[] args) {
+        //TODO
+    }
+}
