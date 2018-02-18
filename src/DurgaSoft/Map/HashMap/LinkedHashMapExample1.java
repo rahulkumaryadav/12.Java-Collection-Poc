ec41f290e855c35979e7a21e4e123fd902ed8d00
Rahul kumar <rahulkumar96369@gmail.com>
2018-02-18 12:09:10 +0530

    linkedHashmap added in todo

---
+++ src/DurgaSoft/Map/HashMap/LinkedHashMapExample1.java
@@ -0,0 +1,13 @@
+package DurgaSoft.Map.HashMap;
+
+/**
+ * @Author Rahul Yadav
+ * Dated :2018-02-07
+ * Same as HashMap but insertion order is preserved.
+ */
+
+public class LinkedHashMapExample1 {
+    public static void main(String[] args) {
+        //TODO
+    }
+}
