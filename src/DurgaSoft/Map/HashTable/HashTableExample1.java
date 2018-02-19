91b21cea8693ff1f885b3552b4780f8e80305305
rahul kumar <user10@sunrichtech.com>
2018-02-19 11:04:07 +0530

    hash table is commited

---
+++ src/DurgaSoft/Map/HashTable/HashTableExample1.java
@@ -0,0 +1,16 @@
+package DurgaSoft.Map.HashTable;
+
+/**
+ * @Author Rahul Yadav
+ * Dated 2018-02-19
+ * Constructor:
+ * Hashtable h=new Hashtable();-->create an empty object with default initial capacity 11 and with default fillratio .75 .
+ * Hashtable h=new Hashtable(int initialCapacity)
+ *Hashtable h=new Hashtable()
+ */
+
+public class HashTableExample1 {
+    public static void main(String[] args) {
+
+    }
+}
